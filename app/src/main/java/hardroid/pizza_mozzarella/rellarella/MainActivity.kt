161d85e69acf435d15b247058e7169ef7f66585a
ArtemBatkov <artembatkov2015@gmail.com>
2022-10-25 17:23:48 +0300

    Branch 1. Step 2.
    Changes:
    - Made RecyclerView as the basic View of this app
    - Uploaded pizza cards to my google photo
    - Read images from album
    - other things + refactoring
    
    Future steps:
    - Make RecyclerView more efficient
    - Add new view or multiple recycler view
    
    Other:

--- app/src/main/java/hardroid/pizza_mozzarella/rellarella/MainActivity.kt
+++ app/src/main/java/hardroid/pizza_mozzarella/rellarella/MainActivity.kt
@@ -3,18 +3,41 @@ package hardroid.pizza_mozzarella.rellarella
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import hardroid.pizza_mozzarella.rellarella.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: PizzasAdapter
+
+
+    private  val pizzaService: PizzaService
+        get() = (applicationContext as App).pizzaService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//         val binding: ViewDataBinding? = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
-        try{
-            var intent = Intent(this,MainScreen::class.java)
-            startActivity(intent)
-            finish()
-        }catch (exception: Exception){
 
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = PizzasAdapter()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+
+        pizzaService.addListener(pizasListener)
     }
+
+    private val pizasListener: PizzasListeners = {
+        adapter.pizzas = it
+    }
+
+
+
 }
